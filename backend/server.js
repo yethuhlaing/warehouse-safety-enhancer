c1041628718f9962dfa2b8562cde3455e7a435c8
Ye Thu Hlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-11-27 20:36:40 +0200

    Configure devops pipeline with Azure

--- backend/server.js
+++ backend/server.js
@@ -132,7 +132,7 @@ server.on('upgrade', (request, socket, head) => {
     }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
